409ae4aa430ae8ff11a3fce363a8d7ac2db991ed
Yaroslav <somsikovyaroslav@yandex.ru>
2025-02-20 11:40:09 +0300

    Add list of resolutions to print

--- print_pow2.py
+++ print_pow2.py
@@ -7,8 +7,5 @@ def print_pow2(num: int):
         pow2= pow2+1
         num=int(num/2)
     print (f"{n} = 2^{pow2} * {num}")
-
-num = input("введите число")
-num = int(num)
-print_pow2(num)
-    
+for num in [600, 720, 768,800, 900, 960, 1050, 1080, 1440, 1280, 1680,1920, 2160, 2560, 3840]:
+    print_pow2(num)
